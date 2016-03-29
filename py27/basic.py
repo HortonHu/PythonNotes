5a8cff16327ef123f1992fa3905f91433f1760e2
Horton <wcbieyuan@gmail.com>
2016-03-29 22:15:03 +0800

    update

---
+++ py27/basic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# IO
+name = raw_input('Please input your name：')
+print 'Hi', name
+
+# Data type: Integer\ Float\ String\ Boolean\ None
+print 'Integer:', 1
+print 'Float  :', 3.14, 1e-5
+print 'String :', 'python'
+print 'String :', 'I\'m OK'
+print 'String :', '\\\n\\'
+print 'String :', '\\\t\\'
+print 'String :', r'\\\t\\'      # 此时\没有转义效果
+print 'String :', '''line1
+... line2'''
+print 'Boolean:', True, not True, True and False, True or False
+print None
+# Data type: list\ tuple
+
+
+# String and Coding
+
+#
