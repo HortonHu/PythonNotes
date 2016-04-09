bfce00ed1433d6924cceaad11eaa542736a8f81f
Horton <wcbieyuan@gmail.com>
2016-04-09 19:52:37 +0800

    update

--- py27/N1_Basic/N05_Function.py
+++ py27/N1_Basic/N05_Function.py
@@ -38,19 +38,20 @@ print power(5, 2)
 
 
 # 默认参数必须指向不变对象！
+# 默认参数是[]，但是函数似乎每次都“记住了”上次添加了'END'后的list
+# 因为默认参数L也是一个变量，它指向对象[] Python函数在定义的时候，默认参数L的值就被计算出来了
+# 每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了
 def add_end(L = []):
     L.append('END')
     return L
 
-# 默认参数是[]，但是函数似乎每次都“记住了”上次添加了'END'后的list
-# 因为默认参数L也是一个变量，它指向对象[]
-# 每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了
 print add_end([1, 2, 3])
 print add_end()
 print add_end()
 print add_end()
 
 
+# 改进写法如下
 def add_end(L = None):
     if L is None:
         L = []
@@ -61,8 +62,8 @@ print add_end()
 print add_end()
 
 
-# 可变参数 可变参数在函数调用时自动组装为一个tuple
-# *args是可变参数，args接收的是一个tuple
+# 可变参数
+# *args是可变参数，args接收的是一个tuple 可变参数在函数调用时自动组装为一个tuple
 # 定义可变参数和定义list或tuple参数相比，仅仅在参数前面加了一个*号。
 def calc(numbers):
     sum = 0
@@ -73,19 +74,19 @@ def calc(numbers):
 print calc([1, 2, 3])
 
 
-def calc(*numbers):
+def calc2(*numbers):
     sum = 0
     for n in numbers:
         sum = sum + n * n
     return sum
 
-print calc(1, 2, 3)
-# 在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传入
+print calc2(1, 2, 3)    # 接收后转为tuple
+# 对于已有的list或tuple 在前面加一个*号，把其中的元素变成可变参数传入
 nums = [1, 2, 3]
-print calc(*nums)
+print calc2(*nums)
 
 
-# 关键字参数 扩展函数的功能
+# 关键字参数 扩展函数的参数 带有参数名字的和参数值的整体
 # **kw是关键字参数，kw接收的是一个dict。
 # 关键字参数在函数内部自动组装为一个dict
 def person(name, age, **kw):
@@ -95,9 +96,9 @@ def person(name, age, **kw):
 person('Michael', 30)
 person('Bob', 35, city='Beijing')
 person('Adam', 45, gender='M', job='Engineer')
-kw = {'city': 'Beijing', 'job': 'Engineer'}
-person('Jack', 24, city=kw['city'], job=kw['job'])
-person('Jack', 24, **kw)
+d = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, city=d['city'], job=d['job'])
+person('Jack', 24, **d)                             # 简化写法
 
 
 # 参数组合
@@ -107,12 +108,13 @@ def func(a, b, c=0, *args, **kw):
 
 func(1, 2)
 func(1, 2, c=3)
-func(1, 2, 3, 'a', 'b')
+func(1, 2, 3, 'a', 'b')             # 此时args接收('a', 'b')
+func(1, 2, 3, 'a', 'b', x=99)       # 此时kw接收{'x': 99}
 
 # 对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的。
 args = (1, 2, 3, 4)
 kw = {'x': 99}
-func(*args, **kw)
+func(*args, **kw)       # 此时args接收(4, )
 
 
 # 递归函数
--- py27/N1_Basic/test.py
+++ py27/N1_Basic/test.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+x = ('a','b','c','d')
+y = {'a':1,'b':2,'c':3}
+
+
+def func(a, b, c=0, *args, **kw):
+    print a
+    print b
+    print c
+    print args
+    print kw
+
+func(1, 2, 3, x, *y)
