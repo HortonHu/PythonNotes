877f2ab21d53f2aa966230a0d1a919d4080da72f
Horton <wcbieyuan@gmail.com>
2016-04-13 16:23:44 +0800

    update

--- py27/N1_Basic/N11_Error_Debug_Test.py
+++ py27/N1_Basic/N11_Error_Debug_Test.py
@@ -190,5 +190,128 @@ print 10 / n
 # 运行代码，程序会自动在pdb.set_trace()暂停并进入pdb调试环境，可以用命令p查看变量，或者用命令c继续运行：
 
 
+# 单元测试 unit test
+class Dict(dict):
+
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+# d = Dict(a=1, b=2)
+
+# 编写单元测试
+# 以test开头的方法就是测试方法，不以test开头的方法不被认为是测试方法，测试的时候不会被执行。
+# unittest.TestCase提供了很多内置的条件判断
+# 调用这些方法就可以断言输出是否是我们所期望的。最常用的断言就是assertEquals()
+import unittest
+
+# from mydict import Dict
+
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEquals(d.a, 1)
+        self.assertEquals(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEquals(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEquals(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):           # 期待抛出指定类型的Error
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+# 运行单元测试
+# 编写好单元测试，我们就可以运行单元测试。
+# 最简单的运行方式是在mydict_test.py的最后加上两行代码：
+if __name__ == '__main__':
+    unittest.main()
+
+# 另一种更常见的方法是在命令行通过参数-m unittest直接运行单元测试：
+# python -m unittest mydict_test
+# 这是推荐的做法，因为这样可以一次批量运行很多单元测试，并且，有很多工具可以自动来运行这些单元测试。
+
+
+# setUp与tearDown
+# 单元测试中编写两个特殊的setUp()和tearDown()方法 这两个方法会分别在每调用一个测试方法的前后分别被执行。
+class TestDict(unittest.TestCase):
+    def setUp(self):
+        print 'setUp...'
+
+    def tearDown(self):
+        print 'tearDown...'
+
+# 单元测试的测试用例要覆盖常用的输入组合、边界条件和异常。
+# 单元测试通过了并不意味着程序就没有bug了，但是不通过程序肯定有bug。
+
+
+# 文档测试
+# Python内置的“文档测试”（doctest）模块可以直接提取注释中的代码并执行测试。
+# doctest严格按照Python交互式命令行的输入和输出来判断测试结果是否正确。
+# 只有测试异常的时候，可以用...表示中间一大段烦人的输出。
+class Dict(dict):
+    '''
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    '''
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
+
+# 当模块正常导入时，doctest不会被执行。
+# 只有在命令行运行时，才执行doctest。所以，不必担心doctest会在非测试环境下执行。
+
 
 
