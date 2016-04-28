71ac9de18abfe3a517db0bd58dd7a652a0646e4a
Horton <wcbieyuan@gmail.com>
2016-04-28 10:18:30 +0800

    update

--- py27/N1_Basic/N08_Modules.py
+++ py27/N1_Basic/N08_Modules.py
@@ -187,9 +187,93 @@ struct.unpack('>IH', '\xf0\xf0\xf0\xf0\x80\x80')
 
 
 # hashlib
+# Python的hashlib提供了常见的摘要算法，如MD5，SHA1等等
+# 摘要算法又称哈希算法、散列算法。它通过一个函数，把任意长度的数据转换为一个长度固定的数据串（通常用16进制的字符串表示）。
+# 摘要算法就是通过摘要函数f()对任意长度的数据data计算出固定长度的摘要digest，目的是为了发现原始数据是否被人篡改过
+# 摘要算法之所以能指出数据是否被篡改过，就是因为摘要函数是一个单向函数，计算f(data)很容易，但通过digest反推data却非常困难。
+# 而且，对原始数据做一个bit的修改，都会导致计算出的摘要完全不同。
+# MD5是最常见的摘要算法，速度很快，生成结果是固定的128 bit字节，通常用一个32位的16进制字符串表示。
+# 以常见的摘要算法MD5为例，计算出一个字符串的MD5值
+import hashlib
+
+md5 = hashlib.md5()
+md5.update('how to use md5 in python hashlib?')
+print md5.hexdigest()
+
+# 如果数据量很大，可以分块多次调用update()，最后计算的结果是一样的
+md5 = hashlib.md5()
+md5.update('how to use md5 in ')
+md5.update('python hashlib?')
+print md5.hexdigest()
+
+# 另一种常见的摘要算法是SHA1，调用SHA1和调用MD5完全类似：
+# SHA1的结果是160 bit字节，通常用一个40位的16进制字符串表示
+import hashlib
+
+sha1 = hashlib.sha1()
+sha1.update('how to use sha1 in ')
+sha1.update('python hashlib?')
+print sha1.hexdigest()
+
+# 比SHA1更安全的算法是SHA256和SHA512，不过越安全的算法越慢，而且摘要长度更长。
+# 两个不同的数据通过某个摘要算法得到了相同的摘要？
+# 完全有可能，因为任何摘要算法都是把无限多的数据集合映射到一个有限的集合中。这种情况称为碰撞
+
+# 摘要算法应用
+# 任何允许用户登录的网站都会存储用户登录的用户名和口令。方法是存到数据库表中
+# name    | password
+# --------+----------
+# michael | 123456
+# bob     | abc999
+# alice   | alice2008
+# 如果以明文保存用户口令，如果数据库泄露，所有用户的口令就落入黑客的手里
+# 正确的保存口令的方式是不存储用户的明文口令，而是存储用户口令的摘要，比如MD5
+# 存储MD5的好处是即使运维人员能访问数据库，也无法获知用户的明文口令
+# username | password
+# ---------+---------------------------------
+# michael  | e10adc3949ba59abbe56e057f20f883e
+# bob      | 878ef96e86145580c38c87f0410ad153
+# alice    | 99b1c2188db85afee403b1536010c2c9
+
+# 练习
+# 根据用户输入的口令，计算出存储在数据库中的MD5口令：
+def calc_md5(password):
+    pass
+
+# 设计一个验证用户登录的函数，根据用户输入的口令是否正确，返回True或False
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(user, password):
+    pass
+
+# 由于常用口令的MD5值很容易被计算出来，所以，要确保存储的用户口令不是那些已经被计算出来的常用口令的MD5，
+# 这一方法通过对原始口令加一个复杂字符串来实现，俗称“加盐”
+def calc_md5(password):
+    return get_md5(password + 'the-Salt')
+# 经过Salt处理的MD5口令，只要Salt不被黑客知道，即使用户输入简单口令，也很难通过MD5反推明文口令。
+# 但是如果有两个用户都使用了相同的简单口令比如123456，在数据库中，将存储两条相同的MD5值，这说明这两个用户的口令是一样的。
+# 如果假定用户无法修改登录名，就可以通过把登录名作为Salt的一部分来计算MD5，从而实现相同口令的用户也存储不同的MD5。
+
+# 练习
+# 根据用户输入的登录名和口令模拟用户注册，计算更安全的MD5：
+db = {}
+
+def register(username, password):
+    db[username] = get_md5(password + username + 'the-Salt')
+
+# 根据修改后的MD5算法实现用户登录的验证
+def login(username, password):
+    pass
 
 
 # itertools
+
+
+
 
 
 # XML
