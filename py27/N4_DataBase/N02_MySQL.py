bbb249255069394a62c5d67f3098ed5b5e24a1f1
Horton <wcbieyuan@gmail.com>
2016-05-06 20:41:36 +0800

    update

--- py27/N4_DataBase/N02_MySQL.py
+++ py27/N4_DataBase/N02_MySQL.py
@@ -2,3 +2,12 @@
 # -*- coding:utf-8 -*-
 
 # MySQL
+import MySQLdb
+try:
+    conn=MySQLdb.connect(host='localhost',user='root',passwd='YES',db='test',port=3306)
+    cur=conn.cursor()
+    cur.execute('select * from user')
+    cur.close()
+    conn.close()
+except MySQLdb.Error,e:
+    print "Mysql Error %d: %s" % (e.args[0], e.args[1])
