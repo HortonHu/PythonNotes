ad6baf6d654cab5ea8ea651d3200bb222c5fd60b
Horton <wcbieyuan@gmail.com>
2016-05-12 22:02:35 +0800

    update

--- py27/N5_Web/Flask/horton.py
+++ py27/N5_Web/Flask/horton.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return 'This is the index of the site.'
+
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/user/<username>')
+def show_name(username):
+    return 'Your name is ' + str(username)
+
+
+@app.route('/user/<int:post_id>')
+def show_id(post_id):
+    return 'Your ID is ' + post_id
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.methods == 'GET':
+#         do_the_login()
+#     else:
+
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('show_name', username='Jia Jia')
+
+
+
 
 if __name__ == '__main__':
     app.run()
