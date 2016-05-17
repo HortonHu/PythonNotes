45b410daa9898f8c0b09501333e7553b7e3d208c
Horton <wcbieyuan@gmail.com>
2016-05-17 11:31:25 +0800

    update

--- py27/N5_Web/Flask/Example/flask_exlample.py
+++ py27/N5_Web/Flask/Example/flask_exlample.py
@@ -3,12 +3,13 @@
 
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__)                   # 初始化
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+def home():                             # 视图函数
+    user_agent = request.headers.get('User-Agent')
+    return render_template('home.html', user_agent=user_agent)
 
 
 @app.route('/signin', methods=['GET'])
@@ -20,9 +21,20 @@ def signin_form():
 def signin():
     username = request.form['username']
     password = request.form['password']
-    if username == 'admin' and password=='password':
+    if username == 'admin' and password == 'password':
         return render_template('signin-ok.html', username=username)
     return render_template('form.html', message='Bad username or password', username=username)
+
+
+@app.route('/user/<username>')
+def show_name(username):
+    return '<h1>Your name is %s</h1>' % str(username)
+
+
+@app.route('/<int:user_num>')
+def show_user_num(user_num):
+    return '<h1>Your Number is %s</h1>' % user_num
+
 
 if __name__ == '__main__':
     app.run()
