fd3e5d7c380fd8bb6fe226a7da8156ab2f94003a
Horton <wcbieyuan@gmail.com>
2016-05-19 15:38:35 +0800

    update

--- py27/N1_Basic/N13_Process_Thread.py
+++ py27/N1_Basic/N13_Process_Thread.py
@@ -268,13 +268,286 @@ t2.join()
 
 
 # 进程 vs. 线程
+# 首先，要实现多任务，通常我们会设计Master-Worker模式，Master负责分配任务，Worker负责执行任务，
+# 因此，多任务环境下，通常是一个Master，多个Worker。
+# 如果用多进程实现Master-Worker，主进程就是Master，其他进程就是Worker。
+# 如果用多线程实现Master-Worker，主线程就是Master，其他线程就是Worker。
+# 多进程模式最大的优点就是稳定性高，因为一个子进程崩溃了，不会影响主进程和其他子进程。
+# （当然主进程挂了所有进程就全挂了，但是Master进程只负责分配任务，挂掉的概率低）著名的Apache最早就是采用多进程模式。
+# 多进程模式的缺点是创建进程的代价大，在Unix/Linux系统下，用fork调用还行，在Windows下创建进程开销巨大。
+# 另外，操作系统能同时运行的进程数也是有限的，在内存和CPU的限制下，如果有几千个进程同时运行，操作系统连调度都会成问题。
+# 多线程模式通常比多进程快一点，但是也快不到哪去，而且，多线程模式致命的缺点就是任何一个线程挂掉都可能直接造成整个进程崩溃，
+# 因为所有线程共享进程的内存。在Windows上，如果一个线程执行的代码出了问题，你经常可以看到这样的提示：
+# “该程序执行了非法操作，即将关闭”，其实往往是某个线程出了问题，但是操作系统会强制结束整个进程。
+# 在Windows下，多线程的效率比多进程要高，所以微软的IIS服务器默认采用多线程模式。
+# 由于多线程存在稳定性的问题，IIS的稳定性就不如Apache。
+# 为了缓解这个问题，IIS和Apache现在又有多进程+多线程的混合模式，真是把问题越搞越复杂。
 
+# 线程切换
+# 多任务一旦多到一个限度，就会消耗掉系统所有的资源，结果效率急剧下降，所有任务都做不好。
 
+# 计算密集型 vs. IO密集型
+# 是否采用多任务的第二个考虑是任务的类型。我们可以把任务分为计算密集型和IO密集型。
+# 计算密集型任务的特点是要进行大量的计算，消耗CPU资源，比如计算圆周率、对视频进行高清解码等等，全靠CPU的运算能力。
+# 这种计算密集型任务虽然也可以用多任务完成，但是任务越多，花在任务切换的时间就越多，CPU执行任务的效率就越低，
+# 所以，要最高效地利用CPU，计算密集型任务同时进行的数量应当等于CPU的核心数。
+# 计算密集型任务由于主要消耗CPU资源，因此，代码运行效率至关重要。Python这样的脚本语言运行效率很低，完全不适合计算密集型任务。
+# 对于计算密集型任务，最好用C语言编写。
+# 第二种任务的类型是IO密集型，涉及到网络、磁盘IO的任务都是IO密集型任务，
+# 这类任务的特点是CPU消耗很少，任务的大部分时间都在等待IO操作完成（因为IO的速度远远低于CPU和内存的速度）。
+# 对于IO密集型任务，任务越多，CPU效率越高，但也有一个限度。常见的大部分任务都是IO密集型任务，比如Web应用。
+# IO密集型任务执行期间，99%的时间都花在IO上，花在CPU上的时间很少，
+# 因此，用运行速度极快的C语言替换用Python这样运行速度极低的脚本语言，完全无法提升运行效率。
+# 对于IO密集型任务，最合适的语言就是开发效率最高（代码量最少）的语言，脚本语言是首选，C语言最差。
 
-# 分布式进程
+# 异步IO
+# 考虑到CPU和IO之间巨大的速度差异，一个任务在执行的过程中大部分时间都在等待IO操作，单进程单线程模型会导致别的任务无法并行执行，
+# 因此，我们才需要多进程模型或者多线程模型来支持多任务并发执行。
+# 现代操作系统对IO操作已经做了巨大的改进，最大的特点就是支持异步IO。
+# 如果充分利用操作系统提供的异步IO支持，就可以用单进程单线程模型来执行多任务，这种全新的模型称为事件驱动模型，
+# Nginx就是支持异步IO的Web服务器，它在单核CPU上采用单进程模型就可以高效地支持多任务。
+# 在多核CPU上，可以运行多个进程（数量与CPU核心数相同），充分利用多核CPU。由于系统总的进程数量十分有限，因此操作系统调度非常高效。
+# 用异步IO编程模型来实现多任务是一个主要的趋势。对应到Python语言，单进程的异步编程模型称为协程
 
 
 # 协程
+# 协程，又称微线程，纤程。英文名Coroutine。
+# 子程序，或者称为函数，在所有语言中都是层级调用，比如A调用B，B在执行过程中又调用了C，C执行完毕返回，B执行完毕返回，最后是A执行完毕。
+# 所以子程序调用是通过栈实现的，一个线程就是执行一个子程序。子程序调用总是一个入口，一次返回，调用顺序是明确的。而协程的调用和子程序不同。
+# 协程看上去也是子程序，但执行过程中，在子程序内部可中断，然后转而执行别的子程序，在适当的时候再返回来接着执行。
+# 注意，在一个子程序中中断，去执行其他子程序，不是函数调用，有点类似CPU的中断。比如子程序A、B：
+def A():
+    print '1'
+    print '2'
+    print '3'
+
+def B():
+    print 'x'
+    print 'y'
+    print 'z'
+# 假设由协程执行，在执行A的过程中，可以随时中断，去执行B，B也可能在执行过程中中断再去执行A 结果可能是：
+# 1
+# 2
+# x
+# y
+# 3
+# z
+# 看起来A、B的执行有点像多线程，但协程的特点在于是一个线程执行，那和多线程比，协程有何优势？
+# 最大的优势就是协程极高的执行效率。因为子程序切换不是线程切换，而是由程序自身控制，
+# 因此，没有线程切换的开销，和多线程比，线程数量越多，协程的性能优势就越明显。
+# 第二大优势就是不需要多线程的锁机制，因为只有一个线程，也不存在同时写变量冲突，
+# 在协程中控制共享资源不加锁，只需要判断状态就好了，所以执行效率比多线程高很多。
+# 因为协程是一个线程执行，那怎么利用多核CPU呢？
+# 最简单的方法是多进程+协程，既充分利用多核，又充分发挥协程的高效率，可获得极高的性能。
+# Python对协程的支持还非常有限，用在generator中的yield可以一定程度上实现协程。虽然支持不完全，但已经可以发挥相当大的威力了。
+# 例如 传统的生产者-消费者模型是一个线程写消息，一个线程取消息，通过锁机制控制队列和等待，但一不小心就可能死锁。
+# 如果改用协程，生产者生产消息后，直接通过yield跳转到消费者开始执行，待消费者执行完毕后，切换回生产者继续生产，效率极高：
+import time
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        time.sleep(1)
+        r = '200 OK'
+
+def produce(c):
+    c.next()
+    n = 0
+    while n < 5:
+        n += 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+if __name__ == '__main__':
+    c = consumer()
+    produce(c)
+# 注意到consumer函数是一个generator（生成器），把一个consumer传入produce后：
+# 1. 首先调用c.next()启动生成器；
+# 2. 然后，一旦生产了东西，通过c.send(n)切换到consumer执行；
+# 3. consumer通过yield拿到消息，处理，又通过yield把结果传回；
+# 4. produce拿到consumer处理的结果，继续生产下一条消息；
+# 5. produce决定不生产了，通过c.close()关闭consumer，整个过程结束。
+# 整个流程无锁，由一个线程执行，produce和consumer协作完成任务，所以称为“协程”，而非线程的抢占式多任务。
+# 最后套用Donald Knuth的一句话总结协程的特点：
+# “子程序就是协程的一种特例。”
+
+# gevent
+# Python通过yield提供了对协程的基本支持，但是不完全。而第三方的gevent为Python提供了比较完善的协程支持。
+# gevent是第三方库，通过greenlet实现协程，其基本思想是：
+# 当一个greenlet遇到IO操作时，比如访问网络，就自动切换到其他的greenlet，等到IO操作完成，再在适当的时候切换回来继续执行。
+# 由于IO操作非常耗时，经常使程序处于等待状态，有了gevent为我们自动切换协程，就保证总有greenlet在运行，而不是等待IO。
+# 由于切换是在IO操作时自动完成，所以gevent需要修改Python自带的一些标准库，这一过程在启动时通过monkey patch完成：
+from gevent import monkey; monkey.patch_socket()
+import gevent
+
+def f(n):
+    for i in range(n):
+        print gevent.getcurrent(), i
+
+g1 = gevent.spawn(f, 5)
+g2 = gevent.spawn(f, 5)
+g3 = gevent.spawn(f, 5)
+g1.join()
+g2.join()
+g3.join()
+# 运行结果：
+# <Greenlet at 0x10e49f550: f(5)> 0
+# <Greenlet at 0x10e49f550: f(5)> 1
+# <Greenlet at 0x10e49f550: f(5)> 2
+# <Greenlet at 0x10e49f550: f(5)> 3
+# <Greenlet at 0x10e49f550: f(5)> 4
+# <Greenlet at 0x10e49f910: f(5)> 0
+# <Greenlet at 0x10e49f910: f(5)> 1
+# <Greenlet at 0x10e49f910: f(5)> 2
+# <Greenlet at 0x10e49f910: f(5)> 3
+# <Greenlet at 0x10e49f910: f(5)> 4
+# <Greenlet at 0x10e49f4b0: f(5)> 0
+# <Greenlet at 0x10e49f4b0: f(5)> 1
+# <Greenlet at 0x10e49f4b0: f(5)> 2
+# <Greenlet at 0x10e49f4b0: f(5)> 3
+# <Greenlet at 0x10e49f4b0: f(5)> 4
+# 可以看到，3个greenlet是依次运行而不是交替运行。
+# 要让greenlet交替运行，可以通过gevent.sleep()交出控制权：
+def f(n):
+    for i in range(n):
+        print gevent.getcurrent(), i
+        gevent.sleep(0)
+# 执行结果：
+# <Greenlet at 0x10cd58550: f(5)> 0
+# <Greenlet at 0x10cd58910: f(5)> 0
+# <Greenlet at 0x10cd584b0: f(5)> 0
+# <Greenlet at 0x10cd58550: f(5)> 1
+# <Greenlet at 0x10cd584b0: f(5)> 1
+# <Greenlet at 0x10cd58910: f(5)> 1
+# <Greenlet at 0x10cd58550: f(5)> 2
+# <Greenlet at 0x10cd58910: f(5)> 2
+# <Greenlet at 0x10cd584b0: f(5)> 2
+# <Greenlet at 0x10cd58550: f(5)> 3
+# <Greenlet at 0x10cd584b0: f(5)> 3
+# <Greenlet at 0x10cd58910: f(5)> 3
+# <Greenlet at 0x10cd58550: f(5)> 4
+# <Greenlet at 0x10cd58910: f(5)> 4
+# <Greenlet at 0x10cd584b0: f(5)> 4
+# 3个greenlet交替运行，
+# 把循环次数改为500000，让它们的运行时间长一点，然后在操作系统的进程管理器中看，线程数只有1个。
+# 当然，实际代码里，我们不会用gevent.sleep()去切换协程，而是在执行到IO操作时，gevent自动切换，代码如下：
+from gevent import monkey; monkey.patch_all()
+import gevent
+import urllib2
+
+def f(url):
+    print('GET: %s' % url)
+    resp = urllib2.urlopen(url)
+    data = resp.read()
+    print('%d bytes received from %s.' % (len(data), url))
+
+gevent.joinall([
+        gevent.spawn(f, 'https://www.python.org/'),
+        gevent.spawn(f, 'https://www.yahoo.com/'),
+        gevent.spawn(f, 'https://github.com/'),
+])
+# 运行结果：
+# GET: https://www.python.org/
+# GET: https://www.yahoo.com/
+# GET: https://github.com/
+# 45661 bytes received from https://www.python.org/.
+# 14823 bytes received from https://github.com/.
+# 304034 bytes received from https://www.yahoo.com/.
+# 从结果看，3个网络操作是并发执行的，而且结束顺序不同，但只有一个线程。
+# 使用gevent，可以获得极高的并发性能，但gevent只能在Unix/Linux下运行，在Windows下不保证正常安装和运行。
+# 由于gevent是基于IO切换的协程，所以最神奇的是，我们编写的Web App代码，不需要引入gevent的包，也不需要改任何代码，
+# 仅仅在部署的时候，用一个支持gevent的WSGI服务器，立刻就获得了数倍的性能提升。
+
+
+# 分布式进程
+# 在Thread和Process中，应当优选Process，因为Process更稳定，而且，Process可以分布到多台机器上，而Thread最多只能分布到同一台机器的多个CPU上。
+# Python的multiprocessing模块不但支持多进程，其中managers子模块还支持把多进程分布到多台机器上。
+# 一个服务进程可以作为调度者，将任务分布到其他多个进程中，依靠网络通信。由于managers模块封装很好，
+# 不必了解网络通信的细节，就可以很容易地编写分布式多进程程序。
+# 例子：如果我们已经有一个通过Queue通信的多进程程序在同一台机器上运行，
+# 现在，由于处理任务的进程任务繁重，希望把发送任务的进程和处理任务的进程分布到两台机器上。怎么用分布式进程实现？
+# 原有的Queue可以继续使用，但是，通过managers模块把Queue通过网络暴露出去，就可以让其他机器的进程访问Queue了。
+# 先看服务进程，服务进程负责启动Queue，把Queue注册到网络上，然后往Queue里面写入任务：
+import random, time, Queue
+from multiprocessing.managers import BaseManager
+
+# 发送任务的队列:
+task_queue = Queue.Queue()
+# 接收结果的队列:
+result_queue = Queue.Queue()
+
+# 从BaseManager继承的QueueManager:
+class QueueManager(BaseManager):
+    pass
+
+# 把两个Queue都注册到网络上, callable参数关联了Queue对象:
+QueueManager.register('get_task_queue', callable=lambda: task_queue)
+QueueManager.register('get_result_queue', callable=lambda: result_queue)
+# 绑定端口5000, 设置验证码'abc':
+manager = QueueManager(address=('', 5000), authkey='abc')
+# 启动Queue:
+manager.start()
+# 获得通过网络访问的Queue对象:
+task = manager.get_task_queue()
+result = manager.get_result_queue()
+# 放几个任务进去:
+for i in range(10):
+    n = random.randint(0, 10000)
+    print('Put task %d...' % n)
+    task.put(n)
+# 从result队列读取结果:
+print('Try get results...')
+for i in range(10):
+    r = result.get(timeout=10)
+    print('Result: %s' % r)
+# 关闭:
+manager.shutdown()
+
+# 当我们在一台机器上写多进程程序时，创建的Queue可以直接拿来用，
+# 但是，在分布式多进程环境下，添加任务到Queue不可以直接对原始的task_queue进行操作，
+# 那样就绕过了QueueManager的封装，必须通过manager.get_task_queue()获得的Queue接口添加
+
+# 另一台机器上启动任务进程（本机上启动也可以）：
+import time, sys, Queue
+from multiprocessing.managers import BaseManager
+
+# 创建类似的QueueManager:
+class QueueManager(BaseManager):
+    pass
+
+# 由于这个QueueManager只从网络上获取Queue，所以注册时只提供名字:
+QueueManager.register('get_task_queue')
+QueueManager.register('get_result_queue')
+
+# 连接到服务器，也就是运行taskmanager.py的机器:
+server_addr = '127.0.0.1'
+print('Connect to server %s...' % server_addr)
+# 端口和验证码注意保持与taskmanager.py设置的完全一致:
+m = QueueManager(address=(server_addr, 5000), authkey='abc')
+# 从网络连接:
+m.connect()
+# 获取Queue的对象:
+task = m.get_task_queue()
+result = m.get_result_queue()
+# 从task队列取任务,并把结果写入result队列:
+for i in range(10):
+    try:
+        n = task.get(timeout=1)
+        print('run task %d * %d...' % (n, n))
+        r = '%d * %d = %d' % (n, n, n*n)
+        time.sleep(1)
+        result.put(r)
+    except Queue.Empty:
+        print('task queue is empty.')
+# 处理结束:
+print('worker exit.')
+
 
 
 
