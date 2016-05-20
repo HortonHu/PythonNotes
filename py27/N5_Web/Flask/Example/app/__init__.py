7ad2bbf879e131ea720d252d119c853155fab5d8
Horton <wcbieyuan@gmail.com>
2016-05-20 13:42:06 +0800

    update

--- py27/N5_Web/Flask/Example/app/__init__.py
+++ py27/N5_Web/Flask/Example/app/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 from flask import Flask
@@ -6,12 +5,18 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+# Flask-Login 在程序的工厂函数中初始化
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'         # 安全等级
+login_manager.login_view = 'auth.login'
 
 
 # 工厂函数 接收配置名
@@ -24,10 +29,15 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # 附加路由和自定义的错误页面
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # 附加auth蓝本到程序上
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
