b476d501198327c08248d8600362f5a85a612453
Horton <wcbieyuan@gmail.com>
2016-05-27 20:22:48 +0800

    update

--- py27/N5_Web/Simple_Example/learn_requests.py
+++ py27/N5_Web/Simple_Example/learn_requests.py
@@ -9,3 +9,9 @@ print r.headers['content-type']
 print r.encoding
 print r.text
 print r.json()
+
+r = requests.post('http://httpbin.org/post', data={'key':'value'})
+r = requests.put('http://httpbin.org/put', data={'key':'value'})
+r = requests.delete('http://httpbin.org/delete')
+r = requests.head('http://httpbin.org/get')
+r = requests.options('http://httpbin.org/get')
