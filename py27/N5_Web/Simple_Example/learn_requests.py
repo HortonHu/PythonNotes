e73e8786414b97a5b9ae3a9aedc808554dbef1b2
Horton <wcbieyuan@gmail.com>
2016-05-28 21:06:12 +0800

    update

--- py27/N5_Web/Simple_Example/learn_requests.py
+++ py27/N5_Web/Simple_Example/learn_requests.py
@@ -10,8 +10,12 @@ print r.encoding
 print r.text
 print r.json()
 
-r = requests.post('http://httpbin.org/post', data={'key':'value'})
-r = requests.put('http://httpbin.org/put', data={'key':'value'})
+r = requests.post('http://httpbin.org/post', data={'key': 'value'})
+r = requests.put('http://httpbin.org/put', data={'key': 'value'})
 r = requests.delete('http://httpbin.org/delete')
 r = requests.head('http://httpbin.org/get')
 r = requests.options('http://httpbin.org/get')
+
+payload = {'key1': 'value1', 'key2': 'value2'}
+r = requests.get('http://httpbin.org/get', params=payload)
+print(r.url)
