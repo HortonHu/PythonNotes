a4229517aad4c9f77b54e1ade2b1311bb5ae9387
Horton <wcbieyuan@gmail.com>
2016-07-24 13:25:35 +0800

    sqlite update and add a template of simple spider

--- Basic/12-Database/SQLite/SQLite.py
+++ Basic/12-Database/SQLite/SQLite.py
@@ -5,8 +5,8 @@
 import sqlite3
 conn = sqlite3.connect('test.db')      # 连接到SQLite数据库文件test.db   如果文件不存在，会自动在当前目录创建:
 cursor = conn.cursor()                 # 创建一个Cursor游标 通过Cursor执行SQL语句
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')    # 创建user表:
-cursor.execute('insert into user (id, name) values (\'1\', \'hortonhu\')')              # 插入一条记录
+cursor.execute('''create table user (id varchar(20) primary key, name varchar(20))''')    # 创建user表:
+cursor.execute('insert into user (id, name) values (?, ?)', (1, u'昊天'))              # 插入一条记录
 print 'inset row number is %s' % cursor.rowcount                                         # 通过rowcount获得插入的行数
 cursor.execute('select * from user where id=?', ('1',))                                 # 传入参数执行查询语句
 values = cursor.fetchall()                                                                # 获得查询结果集:
