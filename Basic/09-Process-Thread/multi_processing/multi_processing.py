13c664776b6862296cad0fff76174a32430d3b30
Horton <wcbieyuan@gmail.com>
2016-07-26 14:55:29 +0800

    update

--- Basic/09-Process-Thread/multi_processing/multi_processing.py
+++ Basic/09-Process-Thread/multi_processing/multi_processing.py
@@ -16,6 +16,7 @@ if pid == 0:
 else:
     print 'I (%s) just created a child process (%s).' % (os.getpid(), pid)
 
+
 # multiprocessing模块是跨平台版本的多进程模块。
 # multiprocessing模块提供了一个Process类来代表一个进程对象，下面的例子演示了启动一个子进程并等待其结束：
 # 创建子进程时，只需要传入一个执行函数和函数的参数，创建一个Process实例，用start()方法启动
