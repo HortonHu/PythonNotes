eda3e4beed552ddc448b1e60f391e2f2f4f9af6f
Horton <wcbieyuan@gmail.com>
2016-07-29 16:58:34 +0800

    OOP __x__ update

---
+++ Basic/07-OOP/__x__/eq_ne_lt_gt_le_ge.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+
+# __eq__(self, other)      # 定义 == 运算符的行为   equal
+# __ne__(self, other)      # 定义 != 运算符的行为   not equal
+# __lt__(self, other)      # 定义 < 运算符的行为    less than
+# __gt__(self, other)      # 定义 > 运算符的行为    great than
+# __le__(self, other)      # 定义 <= 运算符的行为   less than or equal
+# __ge__(self, other)      # 定义 >= 运算符的行为   great than or equal
