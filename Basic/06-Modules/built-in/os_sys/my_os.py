49b79362062eb68ec81d532c92aa8a827bbbae0b
hortonhu <wcbieyuan@gmail.com>
2016-08-17 20:20:48 +0800

    os模块关于os.path更新

--- Basic/06-Modules/built-in/os_sys/my_os.py
+++ Basic/06-Modules/built-in/os_sys/my_os.py
@@ -72,6 +72,23 @@ print [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1
 # os.path.isabs()       是否为绝对路径
 # os.path.isdir()       是否为目录
 # os.path.isfile()      是否为文件
+# os.path.islink(path)  #判断路径是否为链接
+# os.path.ismount(path)  #判断路径是否为挂载点（）
+# os.path.commonprefix(list) #返回list(多个路径)中，所有path共有的最长的路径。
+# os.path.exists(path)  #路径存在则返回True,路径损坏返回False
+# os.path.lexists  #路径存在则返回True,路径损坏也返回True
+# os.path.expanduser(path)  #把path中包含的"~"和"~user"转换成用户目录
+# os.path.expandvars(path)  #根据环境变量的值替换path中包含的”$name”和”${name}”
+# os.path.normcase(path)  #转换path的大小写和斜杠
+# os.path.normpath(path)  #规范path字符串形式
+# os.path.realpath(path)  #返回path的真实路径
+# os.path.relpath(path[, start])  #从start开始计算相对路径
+# os.path.samefile(path1, path2)  #判断目录或文件是否相同
+# os.path.sameopenfile(fp1, fp2)  #判断fp1和fp2是否指向同一文件
+# os.path.samestat(stat1, stat2)  #判断stat tuple stat1和stat2是否指向同一个文件
+# os.path.walk(path, visit, arg)  #遍历path，进入每个目录都调用visit函数，visit函数必须有
+# os.path.supports_unicode_filenames  #设置是否支持unicode路径名
+
 
 # 练习：编写一个search(s)的函数，
 # 能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出完整路径：
